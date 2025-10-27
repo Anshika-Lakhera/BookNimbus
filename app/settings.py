1ed52234a1452ef14ef8a365a9e5366140d1f3fd
Anshika-Lakhera <84fxq4t68h@privaterelay.appleid.com>
2025-10-27 23:23:50 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -124,11 +124,12 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_HTTPONLY = True
 
-# Email (SMTP) - Use environment variables
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'booknimbus@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'cfpt bvtw cqmd ehja')
 
